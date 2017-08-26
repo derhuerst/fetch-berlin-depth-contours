45b8176477812ed7a988405f45bd42f6b689d971
Jannis R <mail@jannisr.de>
2017-08-27 00:43:41 +0200

    get a tile

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 'use strict'
 
-// todo
+const assertCapabilities = require('./lib/assert-capabilities')
+const getTile = require('./lib/get-tile')
+
+const example = [13.393,52.523,13.395,52.524]
+
+assertCapabilities()
+.then((layer) => {
+	// console.log(layer)
+	return getTile(layer, example)
+})
+.catch((err) => {
+	console.error(err)
+	process.exit(1)
+})
