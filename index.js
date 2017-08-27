712c93a2b766bc6529e39cd6e178c4fea7a3384c
Jannis R <mail@jannisr.de>
2017-08-27 02:55:27 +0200

    compute tiles index

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict'
 
 const assertCapabilities = require('./lib/assert-capabilities')
+const computeTileIndex = require('./lib/compute-tile-index')
 const fetchTile = require('./lib/fetch-tile')
 
+const zoom = 18
 const example = [140883, 85996, 18]
 
 assertCapabilities()
-.then(layer => fetchTile(layer, example, 1200))
+.then((layer) => {
+	const index = computeTileIndex(layer, zoom)
+
+	return fetchTile(layer, example, 500)
+})
 .then(res => res.buffer())
 .then(tile => process.stdout.write(tile))
 .catch((err) => {
